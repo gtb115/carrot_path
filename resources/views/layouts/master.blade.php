89cbf1fc62ee6dca960beea3ee10d568234c9642
Griffin <gtb115@gmail.com>
2017-02-24 20:40:27 -0800

    minor updates

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,28 +6,30 @@
 
     <title>@yeild('title')</title>
 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="enabling volunteers to connects with volunteering events">
     <meta name="keywords" content="volunteer, volunteering, volunteer events, non-profit, charity">
     <meta @yield('meta')>
 
     <!-- Move this to home page when built out.  do not keep in master -->
-    <meta name="robots" contents="follow, index"
+    <meta name="robots" contents="follow, index">
 
     <meta name="author" content="carrot_path">
 
     <!-- any additional css -->
     @yield('header')
 
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
 </head>
 
 <body>
     @include('layouts.nav')
 
+    <!-- page content -->
     <div class="container">
         @yield('content')
     </div>
 
+    <!-- foot/legal -->
     @include('layouts.footer')
 
     <!-- any additional js -->
